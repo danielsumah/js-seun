41a78ef28a4e16dcffa5b5d86faa26ba32aa89b0
danielsumah <danielsumah@gmail.com>
2022-02-28 23:35:38 +0100

    data for seun

--- test.js
+++ test.js
@@ -22,24 +22,3 @@ const details = [
     }
 ]
 
-const getUser = (username,password) => {
-    for (let index = 0; index < users.length; index++) {
-        if (users[index].username ===username && users[index].password ===password ) {
-            return true
-        }
-    }
-
-    return false
-}
-
-const getAccountBalance = (username) =>{
-    for (let index = 0; index < details.length; index++) {
-        if (details[index].username == username) {
-            return details[index].accountBalance
-        }
-        
-    }
-}
-// console.log(getUser("timpel", "123"))
-console.log(getAccountBalance('danielreee'))
-
